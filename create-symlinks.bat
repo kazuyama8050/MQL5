18a32c16571f3d32442443b83d219cf1fcca1a1b
kazuyama8050 <kazuki118050@gmail.com>
2023-12-07 23:16:57 +0900

    コメントアウト

--- create-symlinks.bat
+++ create-symlinks.bat
@@ -1,13 +1,13 @@
 setlocal
 
-set app_dir=C:\\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\2FA8A7E69CED7DC259B1AD86A247F675\MQL5
-set project_dir=C:\\Users\Administrator\metatrader\MQL5
+@REM set app_dir=C:\\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\2FA8A7E69CED7DC259B1AD86A247F675\MQL5
+@REM set project_dir=C:\\Users\Administrator\metatrader\MQL5
 
-mklink /d %project_dir%\Experts\MyExperts %app_dir%\Experts\MyExperts
-mklink /d %project_dir%\Include\MyInclude %app_dir%\Include\MyInclude
-mklink /d %project_dir%\Libraries\MyLibraries %app_dir%\Libraries\MyLibraries
-mklink /d %project_dir%\Scripts\Python %app_dir%\Scripts\Python
-mklink /d %app_dir%\Files\Logs %app_dir%\Logs
+@REM mklink /d %project_dir%\Experts\MyExperts %app_dir%\Experts\MyExperts
+@REM mklink /d %project_dir%\Include\MyInclude %app_dir%\Include\MyInclude
+@REM mklink /d %project_dir%\Libraries\MyLibraries %app_dir%\Libraries\MyLibraries
+@REM mklink /d %project_dir%\Scripts\Python %app_dir%\Scripts\Python
+@REM mklink /d %app_dir%\Files\Logs %app_dir%\Logs
 
 
 
