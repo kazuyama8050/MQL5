abef18230dd4a028dac21408dd894b28e5a27047
kazuyama8050 <kazuki118050@gmail.com>
2023-12-07 23:05:12 +0900

    シンボリックリンクを1箇所ずつ貼る

--- create-symlinks.bat
+++ create-symlinks.bat
@@ -12,7 +12,7 @@ mklink /d %app_dir%\Files\Logs %app_dir%\Logs
 
 
 set app_dir=C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\06B227EA7089F89BDE9A3A1C7E6E6AA7\MQL5
-set project_dir=C:\\Users\Administrator\metatrader\MQL5
+set project_dir=C:\\Users\Administrator\metatrader2\MQL5
 
 mklink /d %project_dir%\Experts\MyExperts %app_dir%\Experts\MyExperts
 mklink /d %project_dir%\Include\MyInclude %app_dir%\Include\MyInclude
