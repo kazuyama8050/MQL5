3d4ec9f06e9a64632c1a4d91d89f953ef5331aa9
kazuyama8050 <kazuki118050@gmail.com>
2023-12-09 18:08:57 +0900

    シンボリックリンク修正

--- create-symlinks.bat
+++ create-symlinks.bat
@@ -14,10 +14,10 @@ setlocal
 set app_dir=C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\06B227EA7089F89BDE9A3A1C7E6E6AA7\MQL5
 set project_dir=C:\\Users\Administrator\metatrader2\MQL5
 
-mklink /d %project_dir%\Experts\MyExperts %app_dir%\Experts\MyExperts
-mklink /d %project_dir%\Include\MyInclude %app_dir%\Include\MyInclude
-mklink /d %project_dir%\Libraries\MyLibraries %app_dir%\Libraries\MyLibraries
-mklink /d %project_dir%\Scripts\Python %app_dir%\Scripts\Python
+mklink /d %app_dir%\Experts\MyExperts %project_dir%\Experts\MyExperts
+mklink /d %app_dir%\Include\MyInclude %project_dir%\Include\MyInclude
+mklink /d %app_dir%\Libraries\MyLibraries %project_dir%\Libraries\MyLibraries
+mklink /d %app_dir%\Scripts\Python %project_dir%\Scripts\Python
 mklink /d %app_dir%\Files\Logs %app_dir%\Logs
 
 
